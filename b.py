48c5ebc890dc65213c17b342ba2d32d2b42bb036
Fellas <neuneu12@naver.com>
2025-01-22 13:46:30 +0900

    vod

--- b.py
+++ b.py
@@ -3,19 +3,20 @@ from bs4 import BeautifulSoup
 import requests
 from datetime import datetime
 
-url = "http://www.daum.net/"
+url = "https://datalab.naver.com/keyword/realtimeList.naver?age=20s"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 rank = 1
+# span - item_title
+results = soup.findAll('span','item-title')
 
-results = soup.findAll('a','link_favorsch')
+search_rank_file = open("rankresult.txt","a")
 
-search_rank_file = open("rankresult.txt","w")
-#
+print(response.txt)
+
 print(datetime.today().strftime("%Y년 %m월 %d일의 실시간 검색어 순위입니다.\n"))
-#
+
 for result in results:
     search_rank_file.write(str(rank)+"위:"+result.get_text()+"\n")
     print(rank,"위 : ",result.get_text(),"\n")
     rank += 1
-
