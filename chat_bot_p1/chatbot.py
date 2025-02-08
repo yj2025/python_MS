47d50478585b6950936b2aeff3f37a431123775a
Fellas <neuneu12@naver.com>
2025-02-09 01:10:55 +0900

    변경

--- chat_bot_p1/chatbot.py
+++ chat_bot_p1/chatbot.py
@@ -1,9 +1,9 @@
 import urllib3, json
 from konlpy.tag import Okt
 
-# 실제 API 키 입력
-openApiURL = "http://aiopen.etri.re.kr:8000/MRCServlet"
-accessKey = "81319711-16d7-47a0-a339-5880a0b3f13e"  # 여기에 실제 키 입력
+# API 키 입력
+openApiURL = "http://aiopen.etri.re.kr:8000/MRCServlet"     #  ETRI의 API에 요청
+accessKey = "81319711-16d7-47a0-a339-5880a0b3f13e"  
 
 # info.txt 파일 경로 확인 후 파일 읽기
 try:
@@ -14,20 +14,23 @@ except FileNotFoundError:
     passage = ''
 
 http = urllib3.PoolManager()
-okt = Okt()
+okt = Okt()     # (Open Korea Text)
 
 def action(q):
-    q = okt.normalize(q)
+    q = okt.normalize(q)    #  정규화
 
     if len(okt.nouns(q)):
         requestJson = {"argument": {"question": q, "passage": passage}}
         response = http.request("POST", openApiURL,
                                 headers={"Content-Type": "application/json; charset=UTF-8", "Authorization": accessKey},
                                 body=json.dumps(requestJson))
+        
+        # 서버로부터 받은 응답을 JSON 형식으로 변환
         response = json.loads(str(response.data, "utf-8"))
 
+        # 서버에서 응답한 데이터 중 'confidence' 값 확인   ->   신뢰도가 0.10보다 높으면 그에 해당하는 답변 생성
         if float(response['return_object']['MRCInfo']['confidence']) > 0.10:
-            answer = response['return_object']['MRCInfo']['answer']
+            answer = response['return_object']['MRCInfo']['answer'] #  실제 답변을 받아오기
             if '[END]' in answer:
                 answer = answer[:answer.index('[END]')]
 
--- chat_bot_p1/main.py
+++ chat_bot_p1/main.py
@@ -1,4 +1,4 @@
-# konlpy 라이브러리 설치 한국어 자연어 처리를 위한 파이썬 라이브러리
+  # konlpy 라이브러리 설치 한국어 자연어 처리를 위한 파이썬 라이브러리
 # import sys
 # import subprocess
 
@@ -16,13 +16,16 @@ import jpype
 if not jpype.isJVMStarted():
     jpype.startJVM(r"C:\Program Files\Java\jdk-21\bin\server\jvm.dll")
 
+# 객체 생성
 app = Flask(__name__)
 board = []
 
+# 특정 URL 경로에 대한 처리를 정의
 @app.route('/')
 def index():
     return render_template('main.html', rows=board, qnas=chatbot.src)
 
+# POST 요청 처리
 @app.route('/question', methods=["POST"])
 def question():
     if request.method == "POST":
@@ -31,5 +34,10 @@ def question():
     else:
         return render_template("main.html", rows=board, qnas=chatbot.src)
 
+# 실행
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=8888)
+    app.run(
+        host='0.0.0.0', # 모든 IP에서 접근 가능하도록 설정 (기본값 : '127.0.0.1'(localhost))
+        debug=True,     # 디버그 모드 활성화 (코드 변경 시 자동 재시작, 에러 상세 출력)
+        port=8888       # 8888번 포트에서 서버 실행 (기본값: 5000)
+    )
