e0eba74478e35a174e3ab0d83ffd4809435d6bfb
Fellas <neuneu12@naver.com>
2025-02-09 02:29:22 +0900

    3

---
+++ chat_bot_p3/app.py
@@ -0,0 +1,45 @@
+from openai import OpenAI
+import streamlit as st
+
+st.title("챗봇 프로젝트트")
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+system_message = '''
+너의 이름은 챗봇이야.
+항상 반말로 친근하게 대답해줘.
+영어로 질문을 받아도 무조건 한글로 답변해줘.
+한글이 아닌 답변일 때는 다시 생각해서 꼭 한글로 만들어줘
+'''
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+if len(st.session_state.messages) == 0:
+    st.session_state.messages = [{"role": "system", "content": system_message}]
+
+for idx, message in enumerate(st.session_state.messages):
+    if idx > 0:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ], # type: ignore
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response}) # type: ignore
