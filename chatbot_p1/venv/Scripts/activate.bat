60ad9ef6001948feb58e396a9cd0d7531fa17e8a
Fellas <neuneu12@naver.com>
2025-02-10 00:43:25 +0900

    add

--- chatbot_p1/venv/Scripts/activate.bat
+++ chatbot_p1/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\python(MS)\chatbot_p1\venv"
+set "VIRTUAL_ENV=C:\python_MS\chatbot_p1\venv"
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
